1bafca5f3548f4f9e1c88ba66814d1da7f0ac282
Gonçalo Pereira <goncalopereira@Not-Internet.local>
2018-03-07 19:41:30 +0000

    Initial Express scaffolding

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+/ GET home page. /
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express live' });
+});
+
+
+/lixo/
+const testJSON = [
+  {
+    name: 'John Smith',
+    username: 'CaptainCode'
+  },
+  {
+    name: 'Jane Doe',
+    username: 'JaneyCakes'
+  }
+];
+
+router.get('/sendjson', (req, res, next) => {
+  res.json(testJSON)
+});
+
+module.exports = router;
